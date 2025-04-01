eadafb00d4ba8db15907fe35fa276f44d9ad3415
mayangsyafitri <mayangsyftr@gmail.com>
2025-04-01 23:33:48 +0700

    Menambahkan file app.py dan tests_app.py

--- Tugas 2/tests/tests_app.py
+++ Tugas 2/tests/tests_app.py
@@ -1,5 +1,21 @@
-from operator import add
+import pytest
+from src.calculator import add, subtract, multiply, divide
 
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
 
-def test_addition():
-  assert add(2, 3) == 5
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 5) == -5
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+    assert multiply(-2, 3) == -6
+
+def test_divide():
+    assert divide(10, 2) == 5
+    assert divide(9, 3) == 3
+
+    with pytest.raises(ValueError):
+        divide(5, 0)
